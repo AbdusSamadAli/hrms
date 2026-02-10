dc9ece1379d5b43379fe78934dea654caf1ef04b
Abdus Samad Ali <154547569+AbdusSamadAli@users.noreply.github.com>
2026-02-10 22:07:50 +0530

    first commit

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://hrms-1-1lm3.onrender.com";
+const BASE_URL = "https://hrms-1-11m3.onrender.com";
 
 export async function getEmployees() {
   const res = await fetch(`${BASE_URL}/employees`);
